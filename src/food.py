f71e9977ce8c2818b9a68bf5cdd97178a83f2b62
nadavfelman <nadavf181@gmail.com>
2018-03-14 23:49:50 +0200

    update

--- src/food.py
+++ src/food.py
@@ -1,11 +1,18 @@
 import pygame
+import colors
+from random import randint
 
 class food(pygame.sprite.Sprite):
     """[summary]
     
     """
-    def __init__(self, location, level, color=None, radius=None):
-        self.location = location
-        self.level = level
-        self.color = color or 
+    def __init__(self, location=None, color=None, radius=None, width=None):
+        self.location = location or pygame.math.Vector2(int(randint(0,960)), int(randint(0,540)))
+        self.color = color or colors.WHITE
+        self.radius = radius or 5
+        self.width = width or 2
+    
+    def draw(self, surface):
+        rect = pygame.draw.circle(surface, self.color, (int(self.location.x), int(self.location.y)), self.radius, 1)
+        surface.fill(self.color, rect)
 
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import snake
 import pygame
 import math
+import food
 
 exit_ = False
 clock = pygame.time.Clock()
@@ -12,6 +13,10 @@ screen = pygame.display.set_mode((960, 540))
 s = snake.snake(pygame.math.Vector2(0,0))
 s.add(300)
 
+foods = []
+for _ in xrange(50):
+    foods.append(food.food())
+
 while not exit_:
     screen.fill((0,0,0))
     for event in pygame.event.get():
@@ -20,6 +25,9 @@ while not exit_:
 
     s.move(pygame.math.Vector2(*pygame.mouse.get_pos()), 20)
     s.draw(screen)
+
+    for f in foods:
+        f.draw(screen)
 
     pygame.display.flip()
     clock.tick(30)
