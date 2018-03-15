14f9c9100f190b15436ab85e5a7dccea41c5f066
nadavfelman <nadavf181@gmail.com>
2018-03-15 21:35:23 +0200

    add colors

--- src/colors.py
+++ src/colors.py
@@ -1,8 +1,27 @@
-WHITE = (255,255,255)
-GRAY = (126,126,126)
-BLACK = (0,0,0)
-RED = ()
-BLUE = ()
-YELLOW = ()
-GREEN = ()
-PURPLE = ()
+# black, white, and grays
+WHITE = (255, 255, 255)
+GRAY126 = (126, 126, 126)
+GRAY66 = (66, 66, 66)
+GRAY40 = (40, 40, 40)
+GRAY25 = (25, 25, 25)
+BLACK = (0, 0, 0)
+
+# reds
+RED_BASIC = (255, 0, 0)
+RED = (220, 48, 75)
+
+# greens
+GREEN_BASIC = (0, 255, 0)
+LIGHT_ARMY_GREEN = (13, 194, 131)
+ARMY_GREEN = (8, 115, 78)
+
+# blues
+BLUE_BASIC = (0, 0, 255)
+BLUE = (0, 65, 255)
+
+# combinations
+YELLOW_BASIC = (255, 255, 0)
+YELLOW = (255, 249, 47)
+PURPLE = (113, 109, 249)
+PINK_SILME = (249, 95, 98)
+ORANGE = (255, 109, 55)
