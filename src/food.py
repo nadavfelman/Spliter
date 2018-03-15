f753aeaefade1492b238e011ada5349e400e6945
nadavfelman <nadavf181@gmail.com>
2018-03-15 18:13:59 +0200

    final changes

--- src/food.py
+++ src/food.py
@@ -6,13 +6,12 @@ class food(pygame.sprite.Sprite):
     """[summary]
     
     """
-    def __init__(self, location=None, color=None, radius=None, width=None):
-        self.location = location or pygame.math.Vector2(int(randint(0,960)), int(randint(0,540)))
-        self.color = color or colors.WHITE
-        self.radius = radius or 5
-        self.width = width or 2
+    def __init__(self, location, color, radius, width):
+        self.location = location
+        self.color = color
+        self.radius = radius
+        self.width = width
     
     def draw(self, surface):
         rect = pygame.draw.circle(surface, self.color, (int(self.location.x), int(self.location.y)), self.radius, 1)
-        surface.fill(self.color, rect)
 
