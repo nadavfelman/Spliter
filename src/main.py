3118c3b5a32e7a298934965d3f019b1aaf6ff91d
nadavfelman <nadavf181@gmail.com>
2018-03-15 18:17:10 +0200

    minor changes

--- src/main.py
+++ src/main.py
@@ -6,19 +6,18 @@ import food
 exit_ = False
 clock = pygame.time.Clock()
 
-
 pygame.init()
 screen = pygame.display.set_mode((960, 540))
 
-s = snake.snake(pygame.math.Vector2(0,0))
+s = snake.snake(pygame.math.Vector2(0, 0))
 s.add(300)
 
 while not exit_:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit_ = True
-            
-    screen.fill((0,0,0))
+
+    screen.fill((0, 0, 0))
 
     s.move(pygame.math.Vector2(*pygame.mouse.get_pos()), 20)
     s.draw(screen)
